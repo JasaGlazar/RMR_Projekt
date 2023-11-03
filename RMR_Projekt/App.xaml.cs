f5b3a49d4e608b836a2d5c9af1db5775c0085119
JasaGlazar <jasa.glazar2@gmail.com>
2023-11-03 13:22:13 +0100

    Add project files.

---
+++ RMR_Projekt/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RMR_Projekt
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
